f1f7d0e374aadb101b7807bc417053b5df900772
matt <yollam.ttam@gmail.com>
2014-10-24 15:15:06 -0400

    Minor changes to python script

--- parseDatabase.py
+++ parseDatabase.py
@@ -7,66 +7,45 @@ import numpy.ma as ma
 
 
 ### We would like to read in the training database and output our feature matrix and answer vector ###
-
 conn = sqlite3.connect('titanicTrain.db')
+cursor = conn.cursor()
 print "Successfully loaded training database..."
 
 #initialize X and y matrices 
-nrows = select count(*) from trainData
-ncols = 7
+cursor.execute("select count(*) from trainData")
+nrowsTuple = cursor.fetchall()
+nrowsTuple = nrowsTuple[0]
+nrows = nrowsTuple[0]
+print nrows
+ncols = 8
 X = np.zeros((nrows,ncols))
 y = np.zeros((nrows,1))
 
 cursor = conn.execute("select * from trainData;")
-
-
-
-
-
-
-
-### Below here, you'll find some potentially useful old code ###
-#let's load the database using this module thing
-conn = sqlite3.connect('matt.db')
-print "Successfully loaded database...";
-
-#let's show the contents of the database
-cursor = conn.execute("select * from wall;")
-print "\n **** DISPLAYING CONTENTS OF TABLE **** \n"
+i = 0;
 for row in cursor:
-    print "PersonID: ", row[0]
-    print "Wall Post: ", row[1]
-print "\n **** DISPLAYED CONTENTES SUCCESSFULLY **** \n";
+    y[i] = row[1]
+    X[i,0] = 1
+    X[i,1] = row[2]
+    sex = row[4]
+    sex = sex.encode('ascii','ignore')
+    if (sex == 'male'):
+        X[i,2] = -0.5
+    else:
+        X[i,2] = .5
 
+    X[i,3] = row[5]
+    X[i,4] = row[6]
+    X[i,5] = row[7]
+    X[i,6] = row[8]
+    X[i,7] = row[9]
+    i = i + 1
 
-#let's add something to the database.
-IDtemp = raw_input("Please enter Person ID:")
-walltemp = raw_input("What is your post?")
-conn.execute("insert into wall values (?, ?)",[IDtemp, walltemp])
-
-cursor = conn.execute("select * from wall;")
-print "\n **** ADDING CONTENT TO TABLE **** \n"
-for row in cursor:
-    print "PersonID: ", row[0]
-    print "Wall Post: ", row[1]
-print "\n **** SUCCESSFULLY ADDED CONTENT **** \n"
-
-PersonLookup = raw_input("Who would you like to look up?")
-#cursor = conn.execute('''select * from wall where "PersonID" = (?)''',PersonLookup)
-cursor = conn.execute('''select * from wall where "PersonID" = ?;''',(PersonLookup,))
-print "\n **** LOOKING UP ENTRIES IN TABLE **** \n"
-for row in cursor:
-    print "Wall Post: ", row[1]
-print "\n **** SUCCESSFULLY LOOKED UP ENTRIES **** \n"
-
-
-savebool = raw_input("Would you like to save changes? (0/1)")
-savebool = int(savebool)
-if (savebool == 1):
-    conn.commit()
-    print "saved changes..."
+print "assembled matrices...closing database"
 conn.close()
 
+
+### OK, so now we have made our features matrix and results matrix###
 
 
 
